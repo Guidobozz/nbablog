aeec903cf4b16f7c946c4a2c05f71de8581b4b57
Guidobozz <guidobozzano2002@gmail.com>
2024-10-10 11:05:58 -0300

    progresscarrousel

--- src/Navbar/mycarrousel.jsx
+++ src/Navbar/mycarrousel.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
-import '../Navbar/mycarrousel.css'; 
+import './mycarrousel.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+function MyCarousel() {
+  const items = [
+    {
+      imgSrc: "https://elordenmundial.com/wp-content/uploads/2020/10/NBA-logo-baloncesto-historia-deporte-estados-unidos.jpg",
+      title: "La cuenta regresiva comienza.",
+      description: "El arranque de la temporada está a la vuelta de la esquina. Analizamos qué esperar de los primeros enfrentamientos y cómo llegan los equipos al inicio del año.",
+    },
+    {
+      imgSrc: "https://media3.s-nbcnews.com/i/newscms/2020_04/3203206/200126-kobe-bryant-cs-551p_94a33c06ae6c4bbebaf59985de472c42.jpg",
+      title: "Kobe Bryant Tributo",
+      description: "Un momento para recordar el legado de Kobe.",
+    },
+    {
+      imgSrc: "https://e00-xlk-ue-marca.uecdn.es/uploads/2024/10/07/670369b5546b7.jpeg",
+      title: "El rey y su hijo",
+      description: "Bronny y su padre ya mostraron sus primeros minutos en cancha juntos",
+    },
+  ];
+
+  return (
+    <div className='dynamic-cards-container'>
+      {items.map((item, index) => (
+        <div key={index} className="card">
+          <img src={item.imgSrc} alt={item.title} className="card-img" />
+          <div className="card-content">
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default MyCarousel;
+
+
+
+
+
+/*
 function MyCarousel() {
   return (
     <Carousel className='carouzel'> 
@@ -38,3 +79,4 @@ function MyCarousel() {
 }
 
 export default MyCarousel;
+*/
